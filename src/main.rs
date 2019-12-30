56ebe5feb136a37f2ffd7cf7721d25b6aab6c599
Michael Schumacher <michael.j.schumacher777@gmail.com>
2019-12-30 11:14:57 -0600

    commit point

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+fn main() {
+    println!(">>> Chapter Four");
+
+    page_66();
+    page_67();
+
+
+    println!("<<< Chapter Four");
+}
+
+fn page_67() {
+    println!(">>> page_67");
+    let s1 = gives_ownership();
+    println!("1 contents of s1:{}", s1);
+    let s2 = String::from("Hello 2");
+    println!("2 contents of s1:{}", s1);
+    println!("2 contents of s2:{}", s2);
+
+    let _s3 = takes_and_gives_back(s2);
+    println!("3 contents of s1:{}", s1);
+//    println!("3 contents of s2:{}", s2);  // Not valid now, was moved into line 19
+    println!("3 contents of s3:{}", _s3);
+    println!("<<< page_67");
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("Hello-gives");
+
+    some_string
+}
+
+fn page_66() {
+    println!(">>> Page_66 function");
+    let my_string = String::from("This is the contents of the string - go figure");
+    takes_ownership(my_string);
+//    println!("Should error here {}", my_string);
+
+    let x=5;
+    makes_copy(x);
+
+    println!("Value of x: {}", x);
+    println!("<<< Page_66 function");
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
